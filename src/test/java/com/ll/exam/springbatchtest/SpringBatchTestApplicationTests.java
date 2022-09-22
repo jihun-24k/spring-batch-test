c67bf2146735b3c94ab32ac2b18d1fede1b3ac84
jihun-24k <gi020477@gmail.com>
2022-09-22 09:36:05 +0900

    Chore: 세팅

---
+++ src/test/java/com/ll/exam/springbatchtest/SpringBatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.springbatchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
