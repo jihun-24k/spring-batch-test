28e83191ca5cd9cacd795496eea0099d7a56b9ce
jihun-24k <gi020477@gmail.com>
2022-09-26 13:14:19 +0900

    Feat: 샘플 상품옵션 추가

--- src/main/java/com/ll/exam/springbatchtest/product/service/ProductService.java
+++ src/main/java/com/ll/exam/springbatchtest/product/service/ProductService.java
@@ -1,21 +1,30 @@
 package com.ll.exam.springbatchtest.product.service;
 
 import com.ll.exam.springbatchtest.product.entity.Product;
+import com.ll.exam.springbatchtest.product.entity.ProductOption;
 import com.ll.exam.springbatchtest.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void create(String name, int price, String makerShopName) {
+    public void create(String name, int price, String makerShopName, List<ProductOption> options) {
         Product product = Product.builder()
                 .name(name)
                 .price(price)
                 .makerShopName(makerShopName)
                 .build();
+
+        productRepository.save(product);
+
+        for ( ProductOption option : options ) {
+            product.addOption(option);
+        }
 
         productRepository.save(product);
     }
